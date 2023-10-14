92714f65a5b6673f98764d9dcc90a297e4083873
Joe Panuncialman <joe@panuncialman.com>
2023-10-14 12:30:09 -0400

    Downgraded nextjs

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
 
 const client = sanityClient({
-  projectId: "i9b5u0my",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-05-03", // Update to the latest API version
   useCdn: true, // Enable CDN caching
 });
